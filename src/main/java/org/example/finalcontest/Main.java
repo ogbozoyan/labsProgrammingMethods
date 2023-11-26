4b5a4a66f419808ca30a22a90c38a37157ef1fa4
ogbozoyan <oganes.bozoyan@mail.ru>
2023-11-26 15:56:57 +0300

    init before final contest of 4th trenirovki

---
+++ src/main/java/org/example/finalcontest/Main.java
@@ -0,0 +1,11 @@
+package org.example.finalcontest;
+
+/**
+ * @author ogbozoyan
+ * @since 26.11.2023
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/example/hw4/Main.java
@@ -0,0 +1,11 @@
+package org.example.hw4;
+
+/**
+ * @author ogbozoyan
+ * @since 26.11.2023
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
