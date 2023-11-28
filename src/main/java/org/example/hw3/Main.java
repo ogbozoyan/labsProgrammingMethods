2a1406dfc06cc720f7c9d295c8b44bc6cbdd939f
ogbozoyan <oganes.bozoyan@mail.ru>
2023-11-28 16:01:08 +0300

    add: quick dijecstra

--- src/main/java/org/example/hw3/Main.java
+++ src/main/java/org/example/hw3/Main.java
@@ -1,8 +1,8 @@
 package org.example.hw3;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.*;
 
@@ -14,8 +14,13 @@ public class Main {
     private static final String spaceSplit = " ";
 
     public static void main(String[] args) throws IOException {
-        var reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new FileReader("src/main/java/org/example/hw3/input.txt"));
+//        BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+//        BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/java/org/example/hw3/output.txt"));
+
+//        var reader = new BufferedReader(new InputStreamReader(System.in));
         var writer = new PrintWriter(System.out);
+
         //init
 
         String[] firstLine = reader.readLine().split(spaceSplit);
@@ -35,238 +40,101 @@ public class Main {
         Graph graph = new Graph(listEdges);
         //end init
 
-        int dijkstra = graph.getDijkstra(from, to);
+        Long dijkstra = graph.getDijkstraFast(from, to);
 
         System.out.println(dijkstra);
-
         writer.close();
         reader.close();
     }
 
     static class Graph {
-        private final HashMap<String, HashMap<String, Integer>> graph;
-        private final Set<String> vertexes;
+        private final List<List<Edge>> graph;
 
+        static class Edge {
+            public String vertex;
+            public Long cost;
+
+            public Edge(String vertex, Long cost) {
+                this.vertex = vertex;
+                this.cost = cost;
+            }
+
+            public String getVertex() {
+                return vertex;
+            }
+
+
+            public Long getCost() {
+                return cost;
+            }
+
+        }
 
         public Graph(ArrayList<String> listEdges) {
-            graph = new HashMap<>();
+            graph = new ArrayList<>();
+
+            for (int i = 0; i < listEdges.size() + 5; i++) {
+                graph.add(new ArrayList<>());
+            }
+
             for (String listEdge : listEdges) {
+
                 String[] s = listEdge.split(" ");
 
-                String key = s[0];
-                String key1 = s[1];
-                int value = Integer.parseInt(s[2]);
-
-                // For first vertex of the edge
-                graph.putIfAbsent(key, new HashMap<>());
-                graph.get(key).put(key1, value);
-
-                // For second vertex of the edge
-                graph.putIfAbsent(key1, new HashMap<>());
-                graph.get(key1).put(key, value);
+                String from = s[0];
+                String to = s[1];
+                long value = Long.parseLong(s[2]);
+                addEdge(from, to, value);
             }
-            vertexes = graph.keySet();
 
         }
 
-        public Graph(List<List<String>> identMatrix) {
-            graph = new HashMap<>();
-            for (int i = 0; i < identMatrix.size(); i++) {
-                String vertex = String.valueOf(i + 1);
-                HashMap<String, Integer> edge = new HashMap<>();
-
-                List<String> row = identMatrix.get(i);
-                for (int j = 0; j < row.size(); j++) {
-                    String destinationVertex = String.valueOf(1 + j);
-                    int cost = Integer.parseInt(row.get(j));
-
-                    if (cost < 0) {
-                        continue;
-                    }
-
-                    edge.put(destinationVertex, cost);
-                }
-
-                graph.put(vertex, edge);
-            }
-            vertexes = graph.keySet();
-
+        private void addEdge(String from, String to, Long cost) {
+            graph.get(Integer.parseInt(from)).add(new Edge(to, cost));
+            graph.get(Integer.parseInt(to)).add(new Edge(from, cost));
         }
 
-        public int getDijkstra(String from, String to) {
-            if(!graph.containsKey(from)){
-                return -1;
-            }
-//            PriorityQueue<iPair> pq = new PriorityQueue<>(V, Comparator.comparingInt(o -> o.first));
-            HashMap<String, Integer> costsHashMap = initBuildCosts(from);
-            HashMap<String, String> parentsHashMap = initBuildParents(from);
 
-            List<String> visited = new ArrayList<>();
-            visited.add(from);
+        //O(E logV)
+        public Long getDijkstraFast(String from, String to) {
+            long[] costs = new long[graph.size() + 1];
+            Arrays.fill(costs, Long.MAX_VALUE);
 
-            String lowestCostNode = findLowestCostNode(costsHashMap, visited);
-            while (lowestCostNode != null) {
-                Integer cost = costsHashMap.get(lowestCostNode);
+            PriorityQueue<Edge> visited = new PriorityQueue<>(Comparator.comparingLong(Edge::getCost));
 
-                HashMap<String, Integer> children = graph.get(lowestCostNode);
-                Set<Map.Entry<String, Integer>> entries = children.entrySet();
+            costs[Integer.parseInt(from)] = 0L;
+            visited.add(new Edge(from, 0L));
+            while (!visited.isEmpty()) {
+                Edge cur = visited.poll();
+                int curVertex = Integer.parseInt(cur.vertex);
 
-                for (Map.Entry<String, Integer> kid : entries) {
-                    String kidKey = kid.getKey();
-                    Integer kidCost = kid.getValue();
-                    if (Objects.equals(kidKey, from) || Objects.equals(kidKey, lowestCostNode)) {
-                        continue;
-                    }
-                    Integer newCost = cost + kidCost;
-                    if (costsHashMap.get(kidKey) > newCost) {
-                        costsHashMap.put(kidKey, newCost);
-                        parentsHashMap.put(kidKey, lowestCostNode);
+                Long curVertexCost = costs[curVertex];
+
+
+                for (Edge kid : graph.get(curVertex)) {
+                    String kidKey = kid.vertex;
+
+                    int kidVertex = Integer.parseInt(kidKey);
+                    Long kidCost = kid.cost;
+
+                    long newCost = curVertexCost + kidCost;
+                    if (costs[kidVertex] > newCost) {
+                        costs[kidVertex] = newCost;
+                        visited.add(new Edge(kidKey, newCost));
                     }
                 }
-                visited.add(lowestCostNode);
-                lowestCostNode = findLowestCostNode(costsHashMap, visited);
             }
 
-            if (costsHashMap.get(to).equals(Integer.MAX_VALUE)) {
-                return -1;
+            if (costs[Integer.parseInt(to)] == Long.MAX_VALUE) {
+                return -1L;
             } else
-                return costsHashMap.get(to);
-        }
-
-        public String getDijkstraPath(String from, String to) {
-
-            HashMap<String, Integer> costsHashMap = initBuildCosts(from);
-            HashMap<String, String> parentsHashMap = initBuildParents(from);
-
-            if (from.equals(to)) {
-                return from;
-            }
-            List<String> visited = new ArrayList<>();
-            visited.add(from);
-
-            String lowestCostNode = findLowestCostNode(costsHashMap, visited);
-            while (lowestCostNode != null) {
-                Integer cost = costsHashMap.get(lowestCostNode);
-
-                HashMap<String, Integer> children = graph.get(lowestCostNode);
-                Set<Map.Entry<String, Integer>> entries = children.entrySet();
-
-                for (Map.Entry<String, Integer> kid : entries) {
-                    String kidKey = kid.getKey();
-                    Integer kidCost = kid.getValue();
-                    if (Objects.equals(kidKey, from) || Objects.equals(kidKey, lowestCostNode)) {
-                        continue;
-                    }
-                    Integer newCost = cost + kidCost;
-                    if (costsHashMap.get(kidKey) > newCost) {
-                        costsHashMap.put(kidKey, newCost);
-                        parentsHashMap.put(kidKey, lowestCostNode);
-                    }
-                }
-                visited.add(lowestCostNode);
-                lowestCostNode = findLowestCostNode(costsHashMap, visited);
-            }
-
-            if (costsHashMap.get(to).equals(Integer.MAX_VALUE)) {
-                return String.valueOf(-1);
-            } else {
-                String node = to;
-                List<String> path = new ArrayList<>();
-                path.add(node);
-                while (node != null) {
-                    node = parentsHashMap.get(node);
-                    if (node != null) {
-                        path.add(node);
-                    }
-                }
-
-                StringBuilder sb = new StringBuilder();
-                for (int i = path.size() - 1; i >= 0; i--) {
-                    sb.append(path.get(i)).append(" ");
-                }
-                return sb.toString();
-            }
-        }
-
-
-        private String findLowestCostNode(HashMap<String, Integer> costs, List<String> visited) {
-            Integer lowestCost = Integer.MAX_VALUE;
-            String lowestCostNode = null;
-            Set<Map.Entry<String, Integer>> nodes = costs.entrySet();
-            for (Map.Entry<String, Integer> node : nodes) {
-                Integer cost = node.getValue();
-                if (cost < lowestCost && !visited.contains(node.getKey())) {
-                    lowestCost = cost;
-                    lowestCostNode = node.getKey();
-                }
-            }
-            return lowestCostNode;
-        }
-
-        /**
-         * Initializes the build costs for each vertex in the graph.
-         *
-         * @param from the starting vertex
-         * @return a HashMap containing the build costs for each vertex
-         */
-        private HashMap<String, Integer> initBuildCosts(String from) {
-            HashMap<String, Integer> costs = new HashMap<>(graph.get(from));
-            Set<String> graphKeys = graph.keySet();
-            for (String graphKey : graphKeys) {
-                if (!costs.containsKey(graphKey)) {
-                    costs.put(graphKey, Integer.MAX_VALUE);
-                }
-            }
-            return costs;
-        }
-
-        /**
-         * Initializes and builds the parent mapping for each vertex in the graph.
-         *
-         * @param from the starting vertex
-         * @return a HashMap containing the parent mapping for each vertex
-         */
-        private HashMap<String, String> initBuildParents(String from) {
-            HashMap<String, Integer> stringIntegerHashMap = graph.get(from);
-            HashMap<String, String> res = new HashMap<>();
-
-            Set<String> fromKeys = graph.get(from).keySet();
-            for (String key : fromKeys) {
-                if (stringIntegerHashMap.get(key) > 0) { //if way exist from "from",put as parent
-                    res.put(key, from);
-                } else { //else put null
-                    res.put(key, null);
-                }
-            }
-            Set<String> keys = graph.keySet();
-            for (String key : keys) {
-                if (!stringIntegerHashMap.containsKey(key)) { //if way exist from "from",put as parent
-                    res.put(key, null);
-                }
-            }
-            return res;
-        }
-
-        /**
-         * Retrieves all vertices in the graph without the specified 'from' vertex.
-         *
-         * @param from the 'from' vertex to exclude from the result
-         * @return a HashMap containing all vertices except the 'from' vertex
-         */
-        private HashMap<String, Integer> getAllWithoutFromVertex(String from) {
-            HashMap<String, Integer> stringIntegerHashMap = graph.get(from);
-            stringIntegerHashMap.remove(from);
-            return stringIntegerHashMap;
+                return costs[Integer.parseInt(to)];
         }
 
         public Object getGraph() {
             return graph;
         }
 
-
-        public Set<String> getVertexes() {
-            return vertexes;
-        }
 
         @Override
         public String toString() {
