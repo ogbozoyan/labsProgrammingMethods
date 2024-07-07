189fe7d304f1a951a86d7f881135f9d9c242858e
ogbozoyan <oganes.bozoyan@mail.ru>
2024-07-07 20:36:43 +0300

    add: leetcode task solution

---
+++ src/main/java/org/example/leetcode/Task125.kt
@@ -0,0 +1,43 @@
+package org.example.leetcode
+
+/*A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+ */
+fun main() {
+    class Solution {
+        fun isPalindrome(s: String): Boolean {
+            val lowercase: String = cleanStrInLowerCase(s)
+
+            return lowercase.equals(lowercase.reversed())
+        }
+
+        private fun cleanStrInLowerCase(s: String) = s.replace(Regex("[^a-zA-Z0-9]"), "").lowercase()
+    }
+    println(Solution().isPalindrome(" "))
+}
