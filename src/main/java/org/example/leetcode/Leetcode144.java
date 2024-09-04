344191020232178fafced9ec232da9b6620158b9
ruabooe <Oganes.BOZOYAN@raiffeisen.ru>
2024-09-04 12:14:55 +0300

    added solved tasks

---
+++ src/main/java/org/example/leetcode/Leetcode144.java
@@ -0,0 +1,30 @@
+package org.example.leetcode;
+
+import java.util.Objects;
+
+/**
+ * @author ogbozoyan
+ * @since 03.09.2024
+ */
+public class Leetcode144 {
+    public static void main(String[] args) {
+
+        System.out.println(new Solution().isPalindrome("aa"));
+
+
+    }
+
+    static class Solution {
+
+        public boolean isPalindrome(String s) {
+            String[] split = s.replaceAll("[^\\p{Alnum}]", "").toLowerCase().split("");
+            int j = split.length - 1;
+            for (int i = 0; i < split.length / 2; i++) {
+                if (Objects.equals(split[i], split[j])) {
+                    j--;
+                } else return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ src/main/java/org/example/leetcode/Leetcode206.java
@@ -0,0 +1,49 @@
+package org.example.leetcode;
+
+/**
+ * @author ogbozoyan
+ * @since 03.09.2024
+ */
+public class Leetcode206 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    static class Solution {
+        public ListNode reverseList(ListNode head) {
+            ListNode prev = null;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+                new Solution().reverseList(
+                        new ListNode(1,
+                                new ListNode(2,
+                                        new ListNode(3,
+                                                new ListNode(4,
+                                                        new ListNode(5)
+                                                )
+                                        )
+                                )
+                        )
+                )
+        );
+    }
+}
+
