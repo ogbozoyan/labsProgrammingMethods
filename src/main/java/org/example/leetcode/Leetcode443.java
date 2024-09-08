f87c6c475bbe58672044455b5aa538e8c91acca2
ruabooe <Oganes.BOZOYAN@raiffeisen.ru>
2024-09-08 13:32:45 +0300

    todo

---
+++ src/main/java/org/example/leetcode/Leetcode443.java
@@ -0,0 +1,24 @@
+package org.example.leetcode;
+
+/**
+ * @author ogbozoyan
+ * @since 06.09.2024
+ */
+public class Leetcode443 {
+
+    static class Solution {
+        public int compress(char[] chars) {
+            int cur = 0;
+            int next = cur + 1;
+
+            for (int i = 0; i < chars.length; i++) {
+
+            }
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().compress(new char[]{'a', 'a', 'b', 'b', 'c', 'c', 'c'}));
+    }
+}
